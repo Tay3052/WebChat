269fc6f279b356d478b84ed696963dc813c8bffc
Takumi <fd3s.bcnr33@gmail.com>
2024-12-19 10:04:44 +0900

    creating chatFunc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,48 @@
 "use client";
 
-import { Box, Button, Center, Link } from "@yamada-ui/react";
+import { useSession } from "next-auth/react";
+import { Box, Button, Center, Link, Text } from "@yamada-ui/react";
 
 export default function Home() {
-  return (
-    <>
-      <Center alignItems={"center"}>
-        <Button>
-          <Link href="/pages/auth/signin">Start Here</Link>
-        </Button>
-      </Center>
+  const { data: session, status } = useSession();
 
-      <Center>
-        <Box as={"p"} marginBottom={"10px"} opacity={0}>
-          <Button type="button">
-            <Link href={""}>SecretChatIsHere🤪</Link>
-          </Button>
-        </Box>
-      </Center>
-    </>
+  if (status === "loading") {
+    return (
+      <>
+        <Center>
+          <Text>Loading...</Text>
+        </Center>
+      </>
+    );
+  }
+
+  if (status === "authenticated" && session?.user) {
+    return (
+      <>
+        <Center marginBottom={"40px"}>
+          <Box>
+            <Text fontSize={"2xl"}>
+              Welcome dear my{" "}
+              <span style={{ color: "red" }}>{session.user.name}</span>
+            </Text>
+          </Box>
+        </Center>
+        <Center>
+          <Box>
+            <Button>
+              <Link href="/pages/chats/chatPage">Start to Chats</Link>
+            </Button>
+          </Box>
+        </Center>
+      </>
+    );
+  }
+
+  return (
+    <Center>
+      <Button>
+        <Link href="/pages/auth/signin">Start Here</Link>
+      </Button>
+    </Center>
   );
 }
